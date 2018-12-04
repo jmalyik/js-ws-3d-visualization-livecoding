cfb2d84ce2f45085c194f23bd4539a75467f6f37
Jozsef Malyik <jozsef.malyik@gmail.com>
2018-12-04 19:14:51 +0100

    drawing a box

---
+++ index.js
@@ -0,0 +1,9 @@
+const config = {
+    baseMapURL: 'https://i.imgur.com/E8vbaRC.png',
+    dataMapURL: 'https://i.imgur.com/MtBe8P6.png',
+    width: 100,
+    height: 100,
+    dataDepth: 10
+};
+const dataVisualizer = new DataVisualizer(config);
+dataVisualizer.init();
